a5fb16cb9bcb90f6bd7080f132a8d82199f8b2c1
TranDucTuan2001 <tuan95373@donga.edu.vn>
2024-06-14 16:33:50 +0700

    upload game

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,162 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <div class="wrapper clearfix">
+      <players
+        :isWinner="isWinner"
+        :scorePlayer="scorePlayer"
+        :activePlayer="activePlayer"
+        :currentScore="currentScore"
+      />
+
+      <controls
+        :isPlaying="isPlaying"
+        :finalScore="finalScore"
+        v-on:handleNewGame="handleNewGame"
+        v-on:handleRollDice="handleRollDice"
+        v-on:handleHoldScore="handleHoldScore"
+        v-on:handleChangeFinalScore="handleChangeFinalScore"
+      />
+      <dices :dices="dices" />
+      <popup-rule
+        :isOpenPopup="isOpenPopup"
+        v-on:handleConfirm="handleConfirm"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import Controls from "./components/Controls.vue";
+import Dices from "./components/Dices.vue";
+import Players from "./components/Players.vue";
+import PopupRule from "./components/PopupRule.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Players, Controls, Dices, PopupRule },
+  name: "App",
+  data() {
+    return {
+      isOpenPopup: false,
+      isPlaying: false,
+      scorePlayer: [0, 0],
+      activePlayer: 0,
+      currentScore: 0,
+      dices: [1, 5],
+      finalScore: 50,
+    };
+  },
+  computed: {
+    isWinner() {
+      let { scorePlayer, finalScore, activePlayer } = this;
+      if (scorePlayer[activePlayer] >= finalScore) {
+        this.isPlaying = false;
+        return true;
+      }
+      return false;
+    },
+  },
+  methods: {
+    handleChangeFinalScore(e) {
+      var number = parseInt(e.target.value);
+
+      if (isNaN(number)) {
+        this.finalScore = "";
+      } else {
+        this.finalScore = number;
+      }
+    },
+    handleNewGame() {
+      this.isOpenPopup = true;
+    },
+    nextPlayer() {
+      this.activePlayer = this.activePlayer === 0 ? 1 : 0;
+      this.currentScore = 0;
+    },
+    handleConfirm() {
+      this.isPlaying = true;
+      this.isOpenPopup = false;
+      this.scorePlayer = [0, 0];
+      this.currentScore = 0;
+      this.dices = [5, 1];
+    },
+    handleRollDice() {
+      if (this.isPlaying) {
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
+        this.dices = [dice1, dice2];
+        if (dice1 === 1 || dice2 === 1) {
+          let activePlayer = this.activePlayer;
+          setTimeout(() => {
+            alert(
+              `Người chơi ${activePlayer + 1} đã quay trúng số 1. Rất tiếc`
+            );
+          }, 10);
+          this.nextPlayer();
+        } else {
+          this.currentScore += dice1 + dice2;
+        }
+      } else {
+        alert("vui lòng nhấn new game!");
+      }
+    },
+    handleHoldScore() {
+      if (this.isPlaying) {
+        let { scorePlayer, activePlayer, currentScore } = this;
+        let scoreOld = scorePlayer[activePlayer];
+        // let cloneScorePlayer = [...scorePlayer];
+        // cloneScorePlayer[activePlayer] = scoreOld + currentScore;
+        // this.scorePlayer = cloneScorePlayer;
+        this.$set(this.scorePlayer, activePlayer, scoreOld + currentScore);
+        if (!this.isWinner) {
+          this.nextPlayer();
+        }
+      } else {
+        alert("vui lòng nhấn new game!");
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/**********************************************
+*** GENERAL
+**********************************************/
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.clearfix::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+body {
+  background-image: linear-gradient(
+      rgba(62, 20, 20, 0.4),
+      rgba(62, 20, 20, 0.4)
+    ),
+    url("assets/back.jpg");
+  background-size: cover;
+  background-position: center;
+  font-family: Lato;
+  font-weight: 300;
+  position: relative;
+  height: 100vh;
+  color: #555;
+}
+
+.wrapper {
+  width: 1000px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  box-shadow: 0px 10px 50px rgba(0, 0, 0, 0.3);
+  overflow: hidden;
 }
 </style>
---
+++ src/components/PopupRule.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="wrapper-popup" v-bind:class="openPopup">
+    <div class="rule">
+      <h3>Luật chơi</h3>
+
+      <ul>
+        <li>
+          Random dư liệu 2 con xúc xắc . Kiểm tra xem người chơi có xoay đúng
+          con số 1 không?
+        </li>
+        <li>Nếu xoay trúng số 1 -> Đổi lượt chơi -> Reset điểm tạm thời</li>
+        <li>Nếu xoay ok, ấn HOLD để dữ điểm</li>
+        <li>Game kết thúc khi có người vượt qua điểm đã nhập vào bạn đầu</li>
+      </ul>
+      <button class="confirm" v-on:click="confirm">Đã hiểu</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "rule",
+  data() {
+    return {};
+  },
+  props: {
+    isOpenPopup: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    confirm() {
+      this.$emit("handleConfirm");
+    },
+  },
+  computed: {
+    openPopup() {
+      return { "open-popup": this.isOpenPopup };
+    },
+  },
+};
+</script>
+<style>
+.wrapper-popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  opacity: 0;
+  visibility: hidden;
+  transition: all 0.3s ease;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+.wrapper-popup.open-popup {
+  opacity: 1;
+  visibility: visible;
+}
+.open-popup .rule {
+  transform: translate(-50%, -50%) scale(1);
+}
+.rule {
+  width: 350px;
+  padding: 20px;
+  font-family: Arial, Helvetica, sans-serif;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) scale(1.2);
+  background-color: #fff;
+  position: absolute;
+  transition: all 0.3s ease;
+}
+.rule h3 {
+  margin-bottom: 10px;
+}
+.rule ul {
+  padding: 15px;
+}
+.rule ul li {
+ padding-bottom: 10px;
+}
+.rule .confirm {
+  margin-top: 10px;
+  cursor: pointer;
+  border: 2px solid #333;
+  padding: 8px 15px;
+  background-color: #fff;
+  transition: all 0.3s ease;
+}
+.rule .confirm:hover {
+  background-color: #333;
+  color: #fff;
+}
+</style>
